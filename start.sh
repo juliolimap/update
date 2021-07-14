578ef49a2705d4c06ba88ee051b3fabaaa1512e8
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-07-14 13:53:10 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+
+sudo chmod -R 777 /home/arcade/shared/frontends
+sudo cp -R attract /home/arcade/shared/frontends
+
+cd attract
+sudo cp -r attract /usr/local/bin
+sudo cp -r attract /usr/bin
+sudo rm -R /home/arcade/Update
+sudo chmod -R 777 /home/arcade/shared/frontends/attract
