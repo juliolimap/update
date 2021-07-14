6247f5b05b976e3fc3ccb3c36aad300b69adfba9
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-07-14 16:22:02 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 sudo chmod -R 777 /home/arcade/shared/frontends
 sudo cp -R attract /home/arcade/shared/frontends
 
-cd attract
+
 sudo cp -R /home/arcade/update/mame /home/arcade/shared/configs
 sudo cp -r /home/arcade/update/mame/attract /usr/local/bin
 sudo cp -r /home/arcade/update/mame/attract /usr/bin
