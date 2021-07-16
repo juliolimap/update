2221b9050effabcd93ad4d9624e2334f5a01a871
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-07-16 14:04:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ sudo cp -r /home/arcade/update/attract/attract /usr/bin
 #sudo cp -r /home/arcade/update/attract/megatech_key.key /home/arcade
 sudo rm -R /home/arcade/update
 sudo rm -R /home/arcade/.local/share/Trash
+sudo pacman -Sy --noconfirm
 sudo chmod -R 777 /home/arcade/shared/frontends/attract
 sleep 6
