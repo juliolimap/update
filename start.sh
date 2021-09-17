23076aa4c1fff63386b3cee69754fc9344e7fb50
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-09-17 09:33:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 
 sudo chmod -R 777 /home/arcade/shared/frontends
 sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
-#sudo cp -R /home/arcade/update/mame /home/arcade/shared/configs
+sudo cp -R /home/arcade/update/mame /home/arcade/shared/configs
 sudo cp -r /home/arcade/update/attract/attract /usr/local/bin
 sudo cp -r /home/arcade/update/attract/attract /usr/bin
 #sudo cp -r /home/arcade/update/attract/megatech_key.key /home/arcade
