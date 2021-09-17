9930be934b09fa6ed38d1ffef92fc8a2f0582d61
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-09-17 18:52:04 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 
-sudo chmod -R 777 /home/arcade/shared/frontends
-sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
-sudo cp -R /home/arcade/update/mame /home/arcade/shared/configs
-sudo cp -r /home/arcade/update/attract/attract /usr/local/bin
-sudo cp -r /home/arcade/update/attract/attract /usr/bin
+#sudo chmod -R 777 /home/arcade/shared/frontends
+#sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
+#sudo cp -R /home/arcade/update/mame /home/arcade/shared/configs
+#sudo cp -r /home/arcade/update/attract/attract /usr/local/bin
+#sudo cp -r /home/arcade/update/attract/attract /usr/bin
 #sudo cp -r /home/arcade/update/attract/megatech_key.key /home/arcade
 #sudo cp -r /home/arcade/update/default.lyt /home/arcade/.qjoypad3
 #sudo cp -r /home/arcade/update/layout /home/arcade/.qjoypad3
