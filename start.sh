2a9ac2f9d5861001482ced3c4574efb2d7eebae7
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-09-21 08:28:25 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
+sudo cp -R /home/arcade/update/retroarch /home/arcade/.config
 
-#sudo chmod -R 777 /home/arcade/shared/frontends
-#sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
-#sudo cp -R /home/arcade/update/mame /home/arcade/shared/configs
-#sudo cp -r /home/arcade/update/attract/attract /usr/local/bin
-#sudo cp -r /home/arcade/update/attract/attract /usr/bin
-#sudo cp -r /home/arcade/update/attract/megatech_key.key /home/arcade
-#sudo cp -r /home/arcade/update/default.lyt /home/arcade/.qjoypad3
-#sudo cp -r /home/arcade/update/layout /home/arcade/.qjoypad3
-#sudo cp -r /home/arcade/update/arcade.lyt /home/arcade/.qjoypad3
-
-#yes | sudo pacman -S groovymame
 sudo cp -r "/home/arcade/update/interactive" /opt/gasetup/core/procedures
 
 sudo rm -R /home/arcade/update
