d46fc6b5b62328c6b6e1a86a275c7545cab5f178
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-10-19 11:42:13 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
 
 sudo cp -r "/home/arcade/update/interactive" /opt/gasetup/core/procedures
 
-sudo cp -R "sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
+sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
 sudo plymouth-set-default-theme -R groovy
 
 sudo rm -R /home/arcade/update
