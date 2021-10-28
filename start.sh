8c0c698ad41ad923398341dc30fefa4d3cf85a92
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-10-28 15:25:46 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@ sudo cp -R /home/arcade/update/retroarch /home/arcade/.config
 sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
 
 sudo cp -r "/home/arcade/update/interactive" /opt/gasetup/core/procedures
+sudo cp -r "/home/arcade/update/attract/attract" /usr/bin
 sudo cp -r "/home/arcade/update/attract/attract" /usr/local/bin
+sudo cp -r "/home/arcade/update/attract/attract" /usr/share
+
 
 #sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
 #sudo plymouth-set-default-theme -R groovy
