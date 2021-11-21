ecbfb1ee3e9ed6a8aa8721bbd902666e5be1bfa5
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-11-21 18:49:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,12 @@ sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
 
 
 
-sudo sed -i -e '$a local bg2 = fe.add_image("/home/arcade/.attract/modules/qr.png", 420, 31, 70, 70 );' "/home/arcade/.attract/layouts/Pandoras Box 6/layout.nut"
+var='local bg2 ='
+var2="local bg2 = fe.add_image("/home/arcade/.attract/modules/qr.png", 410, 25, 80, 80 );\nbg2.preserve_aspect_ratio=true;"
+sed -ri "s@^$var.*@$var2@g" input.txt
  
 sudo cp -r "/home/arcade/update/attract/qr-to-png" /usr/bin
-sudo cp -r "/home/arcade/update/attract/qr-to-png" /usr/local/bin
-sudo cp -r "/home/arcade/update/attract/qr-to-png" /usr/share
+
 
 sudo chmod 777 /usr/bin/qr-to-png
  
