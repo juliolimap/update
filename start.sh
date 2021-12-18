6bda5f852c6a6fb64b17b3b2d69da7d3c00bb91e
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-12-18 14:33:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 #sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
 
 sudo cp -r "/home/arcade/update/config.ini" "/home/arcade/.skyscraper"
+sudo cp -r "/home/arcade/update/interactive" "/opt/gasetup/core/procedures"
 
 var="local bg2 ="
 var2="local bg2 = fe.add_image("/home/arcade/.attract/modules/qr.png", 410, 25, 80, 80 );"
@@ -13,6 +14,7 @@ sed -ri "s@^$var.*@$var2@g" "/home/arcade/.attract/layouts/Pandoras Box 6/layout
  
 sudo cp -r "/home/arcade/update/attract/qr-to-png" "/usr/bin"
 
+sudo cp -r "/home/arcade/update/interactive" "/opt/gasetup/core/procedures"
 
 sudo chmod 777 /usr/bin/qr-to-png
  
@@ -27,12 +29,15 @@ sudo cp -r "/home/arcade/update/attract/attract" "/usr/share"
 
 #sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
 #sudo plymouth-set-default-theme -R groovy
-sudo pkill gasetup
 sudo cp -r "/home/arcade/update/interactive" "/opt/gasetup/core/procedures"
 sleel 5
+sudo cp -r "/home/arcade/update/interactive" "/opt/gasetup/core/procedures"
+
 sudo rm -R /home/arcade/update
 sudo rm -R /home/arcade/.local/share/Trash
 #sudo pacman -Sy --noconfirm
 sudo chmod -R 777 /home/arcade/shared/frontends/attract
-sudo gasetup
+dialog --infobox "Reiniciando..." 10 20 
+sleep 3 
+clear
 reboot
