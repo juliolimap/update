80934cfb1a471234836fca5c10b9fe6b6fdbc5eb
juliolimap <81757489+juliolimap@users.noreply.github.com>
2021-12-24 18:11:56 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,20 +3,19 @@
 #sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
 
 #sudo cp -r "/home/arcade/update/config.ini" "/home/arcade/.skyscraper"
-#sudo cp -R "/home/arcade/update/pacman.d" "/etc/pacman.d"
 sudo cp -r "/home/arcade/update/pacman.conf" "/etc/pacman.conf"
 sudo rm -r /var/lib/pacman/db.lck
 sudo pacman -Sy --noconfirm
 sudo pacman -S libzip --noconfirm
-#var="local bg2 ="
-#var2="local bg2 = fe.add_image(""\"/home/arcade/.attract/modules/qr.png""\", 410, 25, 80, 80 );"
-#sed -ri "s@^$var.*@$var2@g" "/home/arcade/.attract/layouts/Pandoras Box 6/layout.nut"
- 
-#wget https://archlinux.org/packages/extra/x86_64/libzip/download
-#sudo pacman -U --noconfirm /home/arcade/update/download
-dialog --infobox "instalando libzip..." 10 20 
 
-sleep 4
+dialog --infobox "instalando Flycast..." 10 20 
+
+sleep 2
+
+sudo pacman -S fbneo --noconfirm
+
+dialog --infobox "instalando Fbneo..." 10 20
+sleep 2
 
 sudo cp -r "/home/arcade/update/flycast" "/usr/local/bin"
 sleep 2
@@ -27,18 +26,18 @@ sleep 2
 sudo chmod -R 777 /usr/local/bin/flycast
 
  
-#sudo cp -r "/home/arcade/update/attract/qr-to-png" "/usr/bin"
+sudo cp -r "/home/arcade/update/attract/qr-to-png" "/usr/bin"
 sudo mv -f "/home/arcade/update/interactive" "/opt/gasetup/core/procedures"
-#sleep 2
-#sudo chmod 777 /usr/bin/qr-to-png
-#sudo mkdir /home/arcade/shared/frontends/attract/bkp
-#sudo mkdir /home/arcade/shared/frontends/attract/bkp/attract
+sleep 2
+sudo chmod 777 /usr/bin/qr-to-png
+sudo mkdir /home/arcade/shared/frontends/attract/bkp
+sudo mkdir /home/arcade/shared/frontends/attract/bkp/attract
 
 
 
-#sudo cp -r "/home/arcade/update/attract/attract" "/usr/bin"
-#sudo cp -r "/home/arcade/update/attract/attract" "/usr/local/bin"
-#sudo cp -r "/home/arcade/update/attract/attract" "/usr/share"
+sudo cp -r "/home/arcade/update/attract/attract" "/usr/bin"
+sudo cp -r "/home/arcade/update/attract/attract" "/usr/local/bin"
+sudo cp -r "/home/arcade/update/attract/attract" "/usr/share"
 
 #sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
 #sudo plymouth-set-default-theme -R groovy
