9defb751532ad1a8ac142a0591d4e6e08c9b6f00
juliolimap <81757489+juliolimap@users.noreply.github.com>
2022-05-23 18:03:21 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
  
 sudo cp -R /home/arcade/update/retroarch /home/arcade/.config
+sudo cp -R /home/arcade/update/.mame /home/arcade/.config
 sudo cp -R /home/arcade/update/attract /home/arcade/shared/frontends
 #sudo chmod -R 777 /tmp
 sudo chmod -R 777 /home/arcade/.config
