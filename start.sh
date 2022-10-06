688f4b9e2946df173ded433046a0889197cef093
juliolimap <81757489+juliolimap@users.noreply.github.com>
2022-10-06 19:51:10 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -46,14 +46,14 @@ sudo cp -r "/home/arcade/update/attract/attract" "/usr/bin"
 sudo cp -r "/home/arcade/update/attract/attract" "/usr/local/bin"
 sudo cp -r "/home/arcade/update/attract/attract" "/usr/share"
 sudo chmod -R 777 /usr/local/bin/attract
-sudo cp -r "/home/arcade/update/attract/wd.png" "/usr/share/plymouth/themes/groovy"
+ 
+ 
+
+
+
+
+sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
 sudo plymouth-set-default-theme -R groovy
-
-
-
-
-#sudo cp -r "/home/arcade/update/splash.png" /usr/share/plymouth/themes/groovy
-#sudo plymouth-set-default-theme -R groovy
  
 sudo rm -R /home/arcade/update
 sudo rm -R /home/arcade/.local/share/Trash
